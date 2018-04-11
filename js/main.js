b66317b51b8db0fbd2c9e423c7a8f4fe41899e47
bellegis <ilipton@wisc.edu>
2018-04-11 15:21:15 -0500

    added comment

--- js/main.js
+++ js/main.js
@@ -1,8 +1,7 @@
-//DEFINE CUSTOM TILING SCHEME ALLOWING FOR CLOSE ZOOM TO MAP DATA
-
 // DECLARE MAP IN GLOBAL SCOPE TO GET SOME THINGS WORKING
 $(document).ready(function() {
 
+//DEFINE CUSTOM TILING SCHEME ALLOWING FOR CLOSE ZOOM TO MAP DATA
 var crs = new L.Proj.CRS('EPSG:3857', '+proj=merc +a=6378137 +b=6378137 +lat_ts=0.0 +lon_0=0.0 +x_0=0.0 +y_0=0 +k=1.0 +units=m +nadgrids=@null +wktext  +no_defs', {
     origin: [-2.00377E7 , 3.02411E7 ],
     resolutions: [
